5fa0b9f095bacfc0a01f013acc8593943bcbd97e
Vida György Márk <h881215@stud.u-szeged.hu>
2022-04-03 17:35:53 +0200

    implement basic "new game" creation

--- public/client.js
+++ public/client.js
@@ -4,3 +4,24 @@ function hallo(name){
     socket.emit('connectPlayer', {playerName: name});
 }
 
+function newGame(gameId){
+    socket.emit('newGame', { gameId: gameId });
+}
+
+socket.on('newGameReady', (payload) => {
+    console.log(payload.gameId);
+});
+
+$('#new-game-btn').on('click', function(){
+    newGame($('#game-id').val());
+})
+
+
+$('#start-btn').on('click', function() {
+    console.log("A");
+    var name = $('#playerId').val();
+    console.log(name);
+    newGame(name);
+})
+
+
